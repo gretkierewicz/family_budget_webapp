3493e2cfa52e64ba67ca95f908e0133d053b4fd0
Piotr Gretkierewicz <piotr.gretkierewicz@gmail.com>
2022-07-03 16:45:28 +0200

    Added view and serializer for User registration

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+"""
+Source: https://medium.com/django-rest/django-rest-framework-login-and-register-user-fd91cf6029d5
+Reduced to minimal required functionalities
+"""
 
-# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import AllowAny
+
+from backend.auth.serializers import RegisterSerializer
+
+
+class RegisterView(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
