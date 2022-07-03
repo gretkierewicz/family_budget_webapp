4c98818a0fe3992d6ee4b97afaa0afa83b971ef2
Piotr Gretkierewicz <piotr.gretkierewicz@gmail.com>
2022-07-03 22:09:11 +0200

    Added many to many model for sharing budgets

--- backend/finances/models.py
+++ backend/finances/models.py
@@ -6,3 +6,14 @@ class Budget(models.Model):
     owner = models.ForeignKey(
         "auth.User", related_name="budgets", on_delete=models.CASCADE
     )
+    shared_with = models.ManyToManyField(
+        "auth.User",
+        blank=True,
+        related_name="shared_budgets",
+        through="BudgetSharedWith",
+    )
+
+
+class BudgetSharedWith(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
